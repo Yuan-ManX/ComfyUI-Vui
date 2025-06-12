51b184f6843424d99349f51b5a11ddadd0b9eed6
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-06-12 11:32:57 +0800

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1,1 +1,27 @@
+import torchaudio
+
+from vui.inference import render
+from vui.model import Vui
+
+
+class LoadVuiModel:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "model_path": ("STRING", {"default": "fluxions/vui"}),
+                "device": (["cuda", "cpu"], {"default": "cuda"}),
+            }
+        }
+
+    RETURN_TYPES = ("MODEL",)
+    RETURN_NAMES = ("model",)
+    FUNCTION = "load_model"
+    CATEGORY = "Vui"
+
+    def load_model(self, model_path, device):
+        model = Vui.from_pretrained(model_path).cuda()
+        
+        return (model,)
+
 

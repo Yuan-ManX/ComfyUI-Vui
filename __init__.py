d39fede5ed6984483d54e8adefe97b0cf6120abc
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-06-12 11:55:32 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,17 @@
+from .nodes import LoadVuiModel, LoadVuiPrompt, Vui, SaveVui
 
+NODE_CLASS_MAPPINGS = {
+    "LoadVuiModel": LoadVuiModel,
+    "LoadVuiPrompt": LoadVuiPrompt,
+    "Vui": Vui,
+    "SaveVui": SaveVui,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadVuiModel": "Load Vui Model",
+    "LoadVuiPrompt": "Load Vui Prompt",
+    "Vui": "Vui",
+    "SaveVui": "Save Vui",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
